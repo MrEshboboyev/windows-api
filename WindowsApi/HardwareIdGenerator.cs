5d27fc0b046923a1049d6fdbaa2be8b0bccf6dcb
MrEshboboyev <mreshboboyev@gmail.com>
2025-12-04 14:38:37 +0500

    Add project files.

---
+++ WindowsApi/HardwareIdGenerator.cs
@@ -0,0 +1,57 @@
+﻿using System.Management;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace WindowsApi;
+
+public static class HardwareIdGenerator
+{
+    public static string GetUniqueHardwareId()
+    {
+        string cpuId = GetWmiProperty("Win32_Processor", "ProcessorId");
+        string biosId = GetWmiProperty("Win32_BIOS", "SerialNumber");
+        string baseBoardId = GetWmiProperty("Win32_BaseBoard", "SerialNumber");
+        // optional: add disk serial number
+        // string diskId = GetWmiProperty("Win32_DiskDrive", "SerialNumber");
+
+        string rawId = $"{cpuId}-{biosId}-{baseBoardId}";
+
+        return GetHash(rawId);
+    }
+
+    private static string GetWmiProperty(string className, string propertyName)
+    {
+        string result = "";
+        try
+        {
+            ManagementObjectSearcher searcher = new($"SELECT {propertyName} FROM {className}");
+            foreach (ManagementObject obj in searcher.Get())
+            {
+                var value = obj[propertyName];
+                if (value != null)
+                {
+                    result = value.ToString();
+                    break;
+                }
+            }
+        }
+        catch (Exception)
+        {
+            return "UNKNOWN";
+        }
+        return result;
+    }
+
+    private static string GetHash(string text)
+    {
+        byte[] inputBytes = Encoding.ASCII.GetBytes(text);
+        byte[] hashBytes = MD5.HashData(inputBytes);
+
+        StringBuilder sb = new();
+        for (int i = 0; i < hashBytes.Length; i++)
+        {
+            sb.Append(hashBytes[i].ToString("X2"));
+        }
+        return sb.ToString();
+    }
+}
---
+++ WindowsApi/Program.cs
@@ -0,0 +1,11 @@
+﻿using WindowsApi;
+
+string myUniqueId = HardwareIdGenerator.GetUniqueHardwareId();
+
+Console.WriteLine("Kompyuterning Hardware IDsi:");
+Console.WriteLine(myUniqueId);
+
+// example: 
+// 4F92E18C9D8F2A3B4C5D6E7F8A9B0C1D
+
+Console.ReadLine();
